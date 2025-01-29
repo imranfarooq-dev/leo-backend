d6f10bc890dfdfb340990fc6515eedc275eebe19
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-01-29 09:27:56 +0000

    :bug: Allow atomic reordering of images

--- supabase/migrations/20250129083500_add_fn_to_update_image_orders.sql
+++ supabase/migrations/20250129083500_add_fn_to_update_image_orders.sql
@@ -1,5 +1,9 @@
 BEGIN;
 
+ALTER TABLE images DROP CONSTRAINT IF EXISTS unique_document_order;
+ALTER TABLE images ADD CONSTRAINT unique_document_order UNIQUE (document_id, "order") DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE images DROP COLUMN IF EXISTS temp_order;
+
 CREATE OR REPLACE FUNCTION update_image_orders(
   image_updates jsonb[]
 ) RETURNS SETOF images AS $$
@@ -7,22 +11,13 @@ BEGIN
   WITH updates AS (
     SELECT 
       (json_element->>'id')::uuid as id,
-      (json_element->>'document_id')::uuid as document_id,
-      json_element->>'image_name' as image_name,
-      json_element->>'image_path' as image_path,
       (json_element->>'order')::int as "order"
     FROM unnest(image_updates) AS arr(json_element)
   )
   UPDATE images i
-  SET temp_order = u.order
+  SET "order" = u.order
   FROM updates u
-  WHERE i.id = u.id
-
-  UPDATE images
-  SET "order" = temp_order,
-      temp_order = NULL
-  FROM updates u
-  WHERE u.id = images.id
+  WHERE i.id = u.id;
 
   -- Return updated records
   RETURN QUERY
