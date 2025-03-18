c0f968f5a0f84541fd89d798f28a58d75efd2750
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-03-18 14:42:45 +0000

    Use filename for images

---
+++ supabase/migrations/20250318144000_use_image_filename.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+ALTER TABLE images ADD COLUMN filename VARCHAR;
+UPDATE images
+SET filename = REGEXP_REPLACE(image_path, '^images/full/', '');
+ALTER TABLE images ALTER COLUMN filename SET NOT NULL;
+ALTER TABLE images DROP COLUMN image_path;
+
+COMMIT;
