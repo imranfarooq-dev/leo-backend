f17344c34590118097cda721b4cdce35fcb2af23
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-04-03 21:41:28 +0100

    Don't have authguard on public routes

--- src/comon/guards/clerk.auth.guard.ts
+++ src/comon/guards/clerk.auth.guard.ts
@@ -6,14 +6,26 @@ import {
 } from '@nestjs/common';
 import { getAuth } from '@clerk/express'; // to retrieve auth data from the request
 import type { ClerkClient } from '@clerk/backend'; // ClerkClient type for typing (optional)
+import { Reflector } from '@nestjs/core';
+import { IsPublic } from '@/src/shared/constant';
 
 @Injectable()
 export class ClerkAuthGuard implements CanActivate {
   constructor(
     @Inject('ClerkClient') private clerkClient: ClerkClient, // inject our Clerk client
+    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IsPublic, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const req = context.switchToHttp().getRequest();
     const auth = getAuth(req);
     const userId = auth.userId; // Clerk populates userId if token is valid
